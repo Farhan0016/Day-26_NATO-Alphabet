88733efbd7747e550f66bb5bab449f10e2e1d0e2
Muhammad Farhan <farhan4main@gmail.com>
2023-04-18 05:17:27 +0500

    updated

--- main.py
+++ main.py
@@ -3,12 +3,21 @@
 
 import pandas as pd
 
+
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word if letter not in [' ', '.']]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
 nato_df = pd.read_csv("nato_phonetic_alphabet.csv")
 
 # Loop through rows of a data frame Keyword Method with iterrows()
 phonetic_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-word = input("Enter a word: ").upper()
-
-output_list = [phonetic_dict[letter] for letter in word if letter not in [' ', '.']]
-print(output_list)
+generate_phonetic()
